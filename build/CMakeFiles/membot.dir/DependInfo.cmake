4493b36ed2055040d6004ccf0181fd7a21e89aed
parallels <parallels@localhost.localdomain>
2020-11-29 09:48:33 -0800

    first commit

---
+++ build/CMakeFiles/membot.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/parallels/projects/chatbot/src/chatbot.cpp" "/home/parallels/projects/chatbot/build/CMakeFiles/membot.dir/src/chatbot.cpp.o"
+  "/home/parallels/projects/chatbot/src/chatgui.cpp" "/home/parallels/projects/chatbot/build/CMakeFiles/membot.dir/src/chatgui.cpp.o"
+  "/home/parallels/projects/chatbot/src/chatlogic.cpp" "/home/parallels/projects/chatbot/build/CMakeFiles/membot.dir/src/chatlogic.cpp.o"
+  "/home/parallels/projects/chatbot/src/graphedge.cpp" "/home/parallels/projects/chatbot/build/CMakeFiles/membot.dir/src/graphedge.cpp.o"
+  "/home/parallels/projects/chatbot/src/graphnode.cpp" "/home/parallels/projects/chatbot/build/CMakeFiles/membot.dir/src/graphnode.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WXUSINGDLL"
+  "__WXGTK3__"
+  "__WXGTK__"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/lib/wx/include/gtk3-unicode-3.1"
+  "/usr/local/include/wx-3.1"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
